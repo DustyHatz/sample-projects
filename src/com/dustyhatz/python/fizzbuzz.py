f81cdac6a9cb1cc7bf98876185c6501d5c4a008d
DustyHatz <dustyhatz21@mac.com>
2020-01-14 10:37:25 -0600

    add fizzbuzz.py

---
+++ src/com/dustyhatz/python/fizzbuzz.py
@@ -0,0 +1,21 @@
+'''
+
+Example solution to the classic "FizzBuzz" problem:
+
+Print numbers 1-100, replace multiples of 3 with "Fizz", multiples of 5 with "Buzz", and multiples of 3 and 5 with "FizzBuzz".
+
+'''
+
+for i in range(1, 101):
+
+	if i % 15 == 0:
+		print("FizzBuzz")
+
+	elif i % 5  == 0:
+		print("Fizz")
+
+	elif i % 3 == 0:
+		print("Buzz")
+
+	else:
+		print(i)
