31fe1ef03987cca61a75812c5b39e9e2703b8ddc
DustyHatz <dustyhatz21@mac.com>
2020-01-16 11:08:56 -0600

    add cho_han func

--- src/com/dustyhatz/python/chance.py
+++ src/com/dustyhatz/python/chance.py
@@ -39,7 +39,7 @@ def flip_coin(bet, call):
       return bet
     
     # LOSS - Heads when calling Tails
-    elif call.lower() == "tails" and flip == 1:
+    elif flip == 1 and call.lower() == "tails":
       print(f"LANDED ON HEADS\nSorry, you lost ${bet}")
       return -bet
     
@@ -49,9 +49,44 @@ def flip_coin(bet, call):
       return -bet
 
     
-money += flip_coin(30, "heads")
-money += flip_coin(30, "heads")
-money += flip_coin(30, "heads")
+def cho_han(bet, call):
+  
+  # Check that player has enough money to place bet
+  if bet > money:
+    print("ERROR: You do not have enough money to place bet!")
+  
+  # Check that player call is either odd or even
+  elif call.lower() != "odd" and call.lower() != "even":
+    print("ERROR: Must call 'odd' or 'even'.")
+  
+  else:
+    # Roll the dice
+    roll = random.randint(1,6) + random.randint(1,6)
+    print(f"Rolling dice...you called {call}...")
 
+    # WIN - Rolled even when calling even
+    if roll % 2 == 0 and call.lower() == "even":
+      print(f"ROLL IS EVEN\nYou win ${bet}")
+      return bet
+
+    # WIN - Rolled odd when calling odd
+    elif roll % 2 != 0 and call.lower() == "odd":
+      print(f"ROLL IS ODD\nYou win ${bet}")
+      return bet
+
+    # LOSS - Rolled even when calling odd
+    elif roll % 2 == 0 and call.lower() == "odd":
+      print(f"ROLL IS EVEN\nSorry, you lost ${bet}")
+      return -bet
+
+    # LOSS - Rolled odd when calling even
+    elif roll % 2 != 0 and call.lower() == "even":
+      print(f"ROLL IS ODD\nSorry, you lost ${bet}")
+      return -bet
+
+
+
+money += flip_coin(30, "heads")
+money += cho_han(10, "even")
 print(f"Your total amount of money is ${money}")
 
