6ced6f01e32b008b92efb3bca8200d924995456f
DustyHatz <dustyhatz21@mac.com>
2020-02-02 15:44:47 -0600

    add comments and new functions

--- src/com/dustyhatz/python/scrabble.py
+++ src/com/dustyhatz/python/scrabble.py
@@ -1,14 +1,17 @@
 
-
+# Dict of elidgible letters to play
 letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
+# Dict of letter point values 
 points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
 
+# Create new dict of letters and points
 letter_to_points = {letter:point for letter, point in zip(letters, points)}
 
+# Set spaces to equal 0 points
 letter_to_points[" "] = 0
 
-
+# Function to score a word
 def score_word(word):
   point_total = 0
   
@@ -17,12 +20,17 @@ def score_word(word):
     
   return point_total
 
+# Test score_word function
 brownie_points = score_word("BROWNIE")
 
+# Create dict of players and words they have played
 player_to_words = {"player1" : ["BLUE", "TENNIS", "EXIT"], "wordNerd" : ["EARTH", "EYES", "MACHINE"], "Lexi Con" : ["ERASER", "BELLY", "HUSKY"], "Prof Reader" : ["ZAP", "COMA", "PERIOD"]}
 
+# Create empty dictionary to store players and their points
 player_to_points = {}
 
+
+# Iterate over the players and their words played and create a new dict of players and current points
 for player, words in player_to_words.items():
   player_points = 0
   
