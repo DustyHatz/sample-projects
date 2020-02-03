47aa301c1110f24468341d4a04406aa40497e963
DustyHatz <dustyhatz21@mac.com>
2020-02-03 10:52:00 -0600

    update functions and comments

--- src/com/dustyhatz/python/scrabble.py
+++ src/com/dustyhatz/python/scrabble.py
@@ -1,15 +1,22 @@
 
 # Dict of elidgible letters to play
-letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
 # Dict of letter point values 
-points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
+points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10, 1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
 
-# Create new dict of letters and points
+# Merge letter and points into a new dict
 letter_to_points = {letter:point for letter, point in zip(letters, points)}
 
 # Set spaces to equal 0 points
 letter_to_points[" "] = 0
+
+# Create dict of players and words they have played
+player_to_words = {}
+
+# Create empty dictionary to store players and their points
+player_to_points = {}
+
 
 # Function to score a word
 def score_word(word):
@@ -20,28 +27,45 @@ def score_word(word):
     
   return point_total
 
-# Test score_word function
-brownie_points = score_word("BROWNIE")
 
-# Create dict of players and words they have played
-player_to_words = {"player1" : ["BLUE", "TENNIS", "EXIT"], "wordNerd" : ["EARTH", "EYES", "MACHINE"], "Lexi Con" : ["ERASER", "BELLY", "HUSKY"], "Prof Reader" : ["ZAP", "COMA", "PERIOD"]}
+# Function to update player's point totals
+def update_point_totals():
+  for player, words in player_to_words.items():
+    player_points = 0
 
-# Create empty dictionary to store players and their points
-player_to_points = {}
+    for word in words:
+      player_points += score_word(word)
 
-
-# Iterate over the players and their words played and create a new dict of players and current points
-for player, words in player_to_words.items():
-  player_points = 0
+    player_to_points[player] = player_points
   
-  for word in words:
-    player_points += score_word(word)
+  return player_to_points
+
+
+# Function takes in a player and a word, scores the word, and updates the player's word list and point total
+def play_word(player, word):
   
-  player_to_points[player] = player_points
-  
+  if player in player_to_words:
+    player_to_words[player].append(word)
+
+  else:
+    new_players = {}
+    new_players[player] = [word]
+    player_to_words.update(new_players)
+    
+  score_word(word)
+  update_point_totals()
+  return player_to_words
+
+
+# Test with different players and words
+print(player_to_words)
+print(player_to_points)
+play_word("player1", "Scrabble")
+play_word("codey", "Absolutely")
+play_word("player1", "Loopy")
+play_word("DustyHatz", "Cash")
+play_word("DustyHatz", "Sweet")
+play_word("DustyHatz", "Totally")
+print(player_to_words)
 print(player_to_points)
 
-# TODO
-# Create function that adds the word to the list of the player's played words
-def play_word(player, word):
-  for player, words in player_to_words.values():
