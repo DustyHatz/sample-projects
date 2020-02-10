4285fc0efa5685f59c974fced359dcd7468098c9
DustyHatz <dustyhatz21@mac.com>
2020-02-09 20:48:57 -0600

    add business class, a second business, and comments

--- src/com/dustyhatz/python/basta_fazoolin.py
+++ src/com/dustyhatz/python/basta_fazoolin.py
@@ -1,7 +1,47 @@
+# Practice building a system of classes to create a working structure for a business.
+# In this case, two restuarants "Basta Fazoolin' With My Heart" and "Take a' Arepa"
+
 from datetime import time
 
+
+# Create a class for the Business
+class Business:
+  
+  # Constructor takes two parameters: the business name and a list of franchises
+  def __init__(self, name, franchises):
+    self.name = name
+    self.franchises = franchises
+    
+  def __repr__(self):
+    return f"{self.name}"
+
+
+# Create a class for the Franchises
+class Franchise:
+  
+  # Constructor takes two parameters: the address, and a list of offered menus
+  def __init__(self, address, menus):
+    self.address = address
+    self.menus = menus
+    
+  def __repr__(self):
+    return f"Restuarant Located at {self.address}"
+  
+  # Function to find out which menus are being offered at a given time
+  def available_menus(self, time):
+    available_menu = []
+
+    for menu in self.menus:
+      if time >= menu.start_time and time <= menu.end_time:
+        available_menu.append(menu)
+
+    return available_menu
+    
+    
+# Create a class for a menu
 class Menu:
   
+  # Constructor take in four paramteres: the menu name, a dict of items and prices, and the start and ending times the menu is offered
   def __init__(self, name, items, start_time, end_time):
     self.name = name
     self.items = items
@@ -11,31 +51,19 @@ class Menu:
   def __repr__(self):
     return f"{self.name} menu available from {self.start_time} to {self.end_time}"
 
+  # Function to calculate the total bill. Takes in a list of the names of the purchased items
   def calculate_bill(self, purchased_items):
     total_price = 0
+
     for item in purchased_items:
       if item in self.items:
         total_price += self.items[item]
-    return f"Total bill: ${total_price:.2f}"
-    
-    
-class Franchise:
-  
-  def __init__(self, address, menus):
-    self.address = address
-    self.menus = menus
-    
-  def __repr__(self):
-    return f"Restuarant Located at {self.address}"
-  
-  def available_menus(self, time):
-    available_menu = []
-    for menu in self.menus:
-      if time >= menu.start_time and time <= menu.end_time:
-        available_menu.append(menu)
-    return available_menu
 
-      
+    return f"Total bill: ${total_price:.2f}"
+  
+  
+# Basta Store
+
 # Brunch menu
 brunch = Menu("brunch", {
   'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00, 'home fries': 4.50, 'coffee': 1.50, 'espresso': 3.00, 'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50
@@ -65,4 +93,20 @@ flagship_store = Franchise("1232 West End Road", menus)
 # Create new Franchise for the new store
 new_installment = Franchise("12 East Mulberry Street", menus)
 
-print(flagship_store.available_menus(1200))
+# Create a new Business
+basta = Business("Basta Fazoolin' with my Heart", [flagship_store, new_installment])
+
+
+# Arepa Store
+
+# Create Menu for Take a' Arepa store
+arepas_menu = Menu("Take a' Arepa", {
+  'arepa pabellon': 7.00, 'pernil arepa': 8.50, 'guayanes arepa': 8.00, 'jamon arepa': 7.50
+}, 1000, 2000)
+
+# Create new Franchise for Take a' Arepa store
+arepas_place = Franchise("189 Fitzgerald Avenue", arepas_menu)
+
+# Create a Business for Take a' Arepa
+arepa = Business("Take a' Arepa", [arepas_place])
+
