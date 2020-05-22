d29dc913c3bb144e02accc06cf43908f0e9f0de0
DustyHatz <dustyhatz21@mac.com>
2020-05-22 14:24:42 -0500

    added function to generate message for traveler and present attractions they might be interested in.

--- src/com/dustyhatz/python/the_boredless_tourist/script.py
+++ src/com/dustyhatz/python/the_boredless_tourist/script.py
@@ -3,7 +3,10 @@
 
 destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
 
-test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
+test_traveler = ['John Test', 'Shanghai, China', ['historical site', 'art']]
+
+attractions = [[] for destination in destinations]
+
 
 
 def get_destination_index(destination):
@@ -20,7 +23,7 @@ def get_traveler_location(traveler):
   return traveler_destination_index
 
 
-  def add_attraction(destination, attraction):
+def add_attraction(destination, attraction):
 
   try:
     destination_index = get_destination_index(destination)
@@ -40,23 +43,49 @@ def find_attractions(destination, interests):
 
 	destination_index = get_destination_index(destination)
 
-  attractions_in_city = attractions[destination_index]
+	attractions_in_city = attractions[destination_index]
 
-  attractions_with_interest = []
+	attractions_with_interest = []
 
-  for attraction in attractions_in_city:
+	for attraction in attractions_in_city:
 
-    possible_attraction = attraction
+		possible_attraction = attraction
 
-    attraction_tags = attraction[1]
+		attraction_tags = attraction[1]
 
-    for interest in interests:
-
-      if interest in attraction_tags:
+		for interest in interests:
+			
+			if interest in attraction_tags:
         
-        attractions_with_interest.append(possible_attraction[0])
+				attractions_with_interest.append(possible_attraction[0])
 
-  return attractions_with_interest
+	return attractions_with_interest
+
+
+def get_attractions_for_traveler(traveler):
+
+	traveler_destination = traveler[1]
+	traveler_interests = traveler[2]
+
+	traveler_attractions = find_attractions(traveler_destination, traveler_interests)
+
+	interests_string = f"Hi {traveler[0]}, we think you'll like these places around {traveler_destination}: "
+
+	for attraction in traveler_attractions:
+
+		if len(traveler_attractions) <= 1: 
+
+			interests_string += attraction + "."
+
+		elif traveler_attractions.index(attraction) == len(traveler_attractions) - 1:
+
+			interests_string += attraction + "."
+
+		else:
+
+			interests_string += attraction + ", "
+
+	return interests_string
 
 
 add_attraction("Los Angeles, USA", ['Venice Beach', ['beach']])
@@ -64,6 +93,8 @@ add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
 add_attraction("Paris, France", ["Arc de Triomphe", ["historical site", "monument"]])
 add_attraction("Shanghai, China", ["Yu Garden", ["garden", "historcical site"]])
 add_attraction("Shanghai, China", ["Yuz Museum", ["art", "museum"]])
+add_attraction("Shanghai, China", ["Your Butt", ["art", "museum"]])
+add_attraction("Shanghai, China", ["Your Mom's Butt", ["art", "museum"]])
 add_attraction("Shanghai, China", ["Oriental Pearl Tower", ["skyscraper", "viewing deck"]])
 add_attraction("Los Angeles, USA", ["LACMA", ["art", "museum"]])
 add_attraction("São Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
@@ -71,5 +102,5 @@ add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]]
 add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
 add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
 
-find_attractions("Los Angeles, USA", ['art'])
+print(get_attractions_for_traveler(test_traveler))
 
