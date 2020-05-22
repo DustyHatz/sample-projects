cf943921a1f4c44de508c5de9aaf5ca392e75b4f
DustyHatz <dustyhatz21@mac.com>
2020-05-22 12:24:51 -0500

    added interest finder logic

--- src/com/dustyhatz/python/the_boredless_tourist/script.py
+++ src/com/dustyhatz/python/the_boredless_tourist/script.py
@@ -32,8 +32,31 @@ def get_traveler_location(traveler):
     return
 
   except ValueError:
-  	
+
     return
+
+
+def find_attractions(destination, interests):
+
+	destination_index = get_destination_index(destination)
+
+  attractions_in_city = attractions[destination_index]
+
+  attractions_with_interest = []
+
+  for attraction in attractions_in_city:
+
+    possible_attraction = attraction
+
+    attraction_tags = attraction[1]
+
+    for interest in interests:
+
+      if interest in attraction_tags:
+        
+        attractions_with_interest.append(possible_attraction[0])
+
+  return attractions_with_interest
 
 
 add_attraction("Los Angeles, USA", ['Venice Beach', ['beach']])
@@ -47,3 +70,6 @@ add_attraction("São Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
 add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]])
 add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
 add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
+
+find_attractions("Los Angeles, USA", ['art'])
+
