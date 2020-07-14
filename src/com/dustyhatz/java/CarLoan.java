6cc29e9f6afa4bbfb27a960d1d32fb9145d939e4
DustyHatz <dustyhatz21@mac.com>
2020-07-14 12:23:22 -0500

    add comments

--- src/com/dustyhatz/java/CarLoan.java
+++ src/com/dustyhatz/java/CarLoan.java
@@ -1,24 +1,33 @@
 /*
-This is a basic car loan calculator!
+Example of a basic car loan calculator!
 By: Dustin Hatzenbuhler
 */
+
+
 public class CarLoan {
+
     public static void main(String[] args) {
         int carLoan = 10000; //dollars
         int loanLength = 3; //years
         int interestRate = 5; //percent
         int downPayment = 200; //dollars
 
+        // Ensure user 
         if (loanLength <= 0 || interestRate <= 0) {
             System.out.println("Error! You must take out a valid car loan.");
+
+        // If the down payment is greater than or equal to the loan, then no loan is required
         } else if (downPayment >= carLoan) {
             System.out.println("No loan required. The car can be paid in full.");
+
+        // Calculate and print the monthly car loan payment
         } else {
             int remainingBalance = carLoan - downPayment;
             int months = loanLength * 12;
             int monthlyBalance = remainingBalance / months;
             int interest = (monthlyBalance * interestRate) / 100;
             int monthlyPayment = monthlyBalance + interest;
+
             System.out.println(monthlyPayment);
 
         }
