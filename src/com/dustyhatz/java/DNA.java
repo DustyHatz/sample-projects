3b0bd8620d1d122c46bd8690db9ca2a19dd3b6ab
DustyHatz <dustyhatz21@mac.com>
2020-07-14 12:32:13 -0500

    add comments and clean up code

--- src/com/dustyhatz/java/DNA.java
+++ src/com/dustyhatz/java/DNA.java
@@ -1,6 +1,9 @@
-package com.dustyhatz;
+// Example program using basic conditionals to identify
+// whether a protein exists in a strand of DNA
 
+package com.dustyhatz;
 import java.util.Scanner;
+
 
 public class Main {
 
@@ -9,34 +12,45 @@ public class Main {
         // Get DNA strand from input
         // Example "ATTAATATGTACTGA"
         String dna;
+
         Scanner input = new Scanner(System.in);
+
         System.out.println("Enter DNA: ");
+
         dna = input.nextLine();
 
-        // Find the length
+        // Find and print the length of the DNA strand
         System.out.println("Length: " + dna.length());
 
-        // Find the start codon
+        // Find and print the start codon
         int start = dna.indexOf("ATG");
+
         System.out.println("Start: " + start);
 
-        // Find the stop codon
+        // Find and print the stop codon
         int stop = dna.indexOf("TGA");
+
         System.out.println("Stop: " + stop);
 
-        // Check that star and stop codons exist
+        // Check that start and stop codons exist
         if (start != -1 && stop != -1) {
+
             System.out.println("Condition 1 and 2 are satisfied.");
         }
 
         // Check that each additional codon is a sequence of 3 nucleotides
         if (start != -1 && stop != -1 && (stop - start) % 3 == 0) {
+
             System.out.println("Condition 1, 2, and 3 are satisfied.");
 
             // Identify and print the protein
             String protein = dna.substring(start, stop + 3);
+
             System.out.println("Protein: " + protein);
+
+        // If there are no protein codons print No Protein detected
         } else {
+
             System.out.println("No protein detected.");
         }
     }
