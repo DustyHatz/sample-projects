d1431713f44ef6cb7692ad5a9b623eb9ee54388d
DustyHatz <dustyhatz21@mac.com>
2020-07-14 12:52:55 -0500

    add comments and clean up code

--- src/com/dustyhatz/java/Main.java
+++ src/com/dustyhatz/java/Main.java
@@ -1,8 +1,15 @@
+// My very first Java program
+
+
 package com.dustyhatz;
+
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+    	// Print "Hello, World"
+        System.out.println("Hello, World");
+
     }
 }
